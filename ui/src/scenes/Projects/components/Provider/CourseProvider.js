cf844957eb27efaad25284b5002420290be1e361
vishnulphb <vishnupillai02@gmail.com>
2017-10-14 23:55:11 -0400

    updated course provider component

--- ui/src/scenes/Projects/components/Provider/CourseProvider.js
+++ ui/src/scenes/Projects/components/Provider/CourseProvider.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
+import EthereumExam from '../Exams/EthereumExam';
+const isShowing = false;
 
 class CourseProvider extends Component {
+   
     render() {
         return (
 
-            <div className="container">
-                <div className="row">
+                <div className="container">
+                    <div className="row">
 
-                    <div className="col-md-6 label">
-                        <label for="inputCourse">
-                            <h2>
-                                <span class="label label-primary">Course Name</span>
-                            </h2>
-                        </label>
+                        <div className="col-md-6 label">
+                            <label for="inputCourse">
+                                <h2>
+                                    <span class="label label-primary">Course Name</span>
+                                </h2>
+                            </label>
+                        </div>
+                        <div className="col-md-6 label">
+                            <input name="inputCourse" className="form-control" />
+                        </div>
                     </div>
-                    <div className="col-md-6 label">
-                        <input name="inputCourse" className="form-control" />
-                    </div>
+                    {/* <input type="submit" className="btn btn-primary pull-right" value="Submit" /> */}
+
+                    <input type="submit" className="btn btn-primary pull-right" value="Submit" onClick={this.setState(isShowing==true)} /> 
+                    <div className="row">
+                     {isShowing && <EthereumExam/>}
+                     {!isShowing && <EthereumExam/>}
+                    </div> 
                 </div>
-
-                <input type="submit" className="btn btn-primary pull-right" value="Submit" />
-            </div>
-           
+                
         );
     }
 }
